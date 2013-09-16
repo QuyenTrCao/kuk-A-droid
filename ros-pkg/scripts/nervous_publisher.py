29aced8a4714ef7b92efc590537e0da32bcd200d
Arn-O <rnd.bertrand@gmail.com>
2013-09-16 22:42:02 +0200

    finalize service

--- ros-pkg/scripts/nervous_publisher.py
+++ ros-pkg/scripts/nervous_publisher.py
@@ -18,23 +18,32 @@ import rospy
 
 # global constants
 DEFAULT_START_MODE = 'aware'
+NODE_NAME = 'nervous_system'
+
+NERVOUS_MODES = ['aware', 'zombie']
 
 def stop_node():
     '''Clean stuff here.'''
     rospy.delete_param('~mode')
     print 'Bye!'
 
-def handle_set_nervous_mode():
+def handle_set_nervous_mode(req):
     '''Handle for nervous_mode setter (aware/zombie).'''
-    print 'Change mode'
+    # TODO warning if already in requested mode
+    if req.nervous_mode in NERVOUS_MODES:
+        rospy.set_param('~mode', req.nervous_mode)
+        rospy.loginfo('Nervous mode set to %s', req.nervous_mode)
+    else:
+        rospy.logerr('Nervous mode not allowed. Possible values are: aware/zombie')
     return SetNervousModeResponse()
 
 def main():
     '''Where everything starts.'''
-    rospy.init_node('nervous_system')
+    rospy.init_node(NODE_NAME)
     rospy.set_param('~mode', DEFAULT_START_MODE)
+    rospy.loginfo('Nervous system started in %s mode, this can be changed using services', DEFAULT_START_MODE)
     rospy.on_shutdown(stop_node)
-    s = rospy.Service('set_nervous_mode', SetNervousMode,
+    s = rospy.Service('~set_nervous_mode', SetNervousMode,
             handle_set_nervous_mode)
     rospy.spin()
 
