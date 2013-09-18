6ef980616ab864f353379852233417b77dcd71d4
Arn-O <rnd.bertrand@gmail.com>
2013-09-18 22:39:09 +0200

    nightly build

--- ros-pkg/scripts/nervous_publisher.py
+++ ros-pkg/scripts/nervous_publisher.py
@@ -14,18 +14,28 @@ program (zombie mode).
 import roslib; roslib.load_manifest('kuk_a_droid')
 
 from kuk_a_droid.srv import *
+from kuk_a_droid.msg import *
 import rospy
 
 # global constants
+# ROS parameters
 DEFAULT_START_MODE = 'aware'
 NODE_NAME = 'nervous_system'
+PUBLISHER_NAME = 'nervous_states'
+PUB_FREQUENCY = 25
 
+# affective parameters
 NERVOUS_MODES = ['aware', 'zombie']
+# note: emotions and behaviors are listed in the following orders
+# emotions = anger frustration fear distress disgust sorrow surprise interest calm boredom joy
+# behaviors = seek-people seek-toy sleep
+EMOTIONS_INIT = [0, 0, 0, 0, 0, 0, 0, 0, 1500, 0, 0] #Calm
+BEHAVIORS_INIT = [0, 0, 1800] #Sleep
 
 def stop_node():
     '''Clean stuff here.'''
     rospy.delete_param('~mode')
-    print 'Bye!'
+    rospy.logwarn('Nervous publisher has stopped - Bye!')
 
 def handle_set_nervous_mode(req):
     '''Handle for nervous_mode setter (aware/zombie).'''
@@ -37,15 +47,36 @@ def handle_set_nervous_mode(req):
         rospy.logerr('Nervous mode not allowed. Possible values are: aware/zombie')
     return SetNervousModeResponse()
 
+def get_nerv_msg(emotions, behaviors):
+    '''Convert emotions and behaviors lists to msg'''
+    emot_param = Emotions(*emotions)
+    behav_param = Behaviors(*behaviors)
+    nerv_msg = NervStates(emot_param, behav_param)
+    return nerv_msg
+
+def nervous_publisher():
+    '''Publisher loop'''
+    pub = rospy.Publisher(PUBLISHER_NAME, NervStates)
+    emotions = EMOTIONS_INIT
+    behaviors = BEHAVIORS_INIT
+    while not rospy.is_shutdown():
+        msg = get_nerv_msg(emotions, behaviors)
+        pub.publish(msg)
+        rospy.sleep(1.0 / PUB_FREQUENCY)
+
 def main():
     '''Where everything starts.'''
     rospy.init_node(NODE_NAME)
-    rospy.set_param('~mode', DEFAULT_START_MODE)
-    rospy.loginfo('Nervous system started in %s mode, this can be changed using services', DEFAULT_START_MODE)
     rospy.on_shutdown(stop_node)
+    rospy.set_param('~mode', DEFAULT_START_MODE)
     s = rospy.Service('~set_nervous_mode', SetNervousMode,
             handle_set_nervous_mode)
-    rospy.spin()
+    rospy.loginfo('Nervous system started in %s mode, this can be changed using services', DEFAULT_START_MODE)
+    
+    try:
+        nervous_publisher()
+    except rospy.ROSInterruptException:
+        pass
 
 if __name__ == '__main__':
     main()
