9f668e626dae8345c34cbd1a74b41bcc58eedc1f
Arn-O <rnd.bertrand@gmail.com>
2013-11-04 18:10:31 +0100

    add stun for sequence display

--- tools/sequence_editor.py
+++ tools/sequence_editor.py
@@ -16,6 +16,7 @@ import argparse
 
 # global constants
 DEF_FREQ = 60
+DEF_NB_JOINTS = 1
 
 def get_parameters():
     ''' Manage the execution parameters'''        
@@ -48,6 +49,7 @@ def parse_kframe_add(line):
     # TODO: add some checks here
     return int(line)
 
+# TODO: manage the is_frame0() check in a postcmd method
 class SequenceEditor(cmd.Cmd):
 
     '''Subclass of the cmd class'''
@@ -57,6 +59,7 @@ class SequenceEditor(cmd.Cmd):
         logging.debug('Call function preloop()')
         # initialize variables
         self.frequency = DEF_FREQ
+        self.nb_joints = DEF_NB_JOINTS
         self.sequence = {'frequency': self.frequency, 'keyframes': {}}
         print("'Crtl+D' or EOF to quit")
         is_frame0(self.sequence)
@@ -76,16 +79,34 @@ class SequenceEditor(cmd.Cmd):
     def do_kframe_add(self, line):
         '''Insert a new keyframe'''
         logging.debug('Call function do_kframe_add()')
-        frame  = parse_kframe_add(line)
-        j1 = float(input('Enter value for j1:'))
-        self.sequence['keyframes'][frame] = j1
+        frame_nb  = parse_kframe_add(line)
+        vals = []
+        for i in range(self.nb_joints):
+            val = float(input(
+                    'Enter value for joint %i / %i: '  %
+                    ((i + 1), self.nb_joints)
+                ))
+            vals.append(val)
+        self.sequence['keyframes'][frame_nb] = vals
         is_frame0(self.sequence)
 
+    def do_seq_disp(self, line):
+        '''Display the sequence of motion with interpolation'''
+        logging.debug('Call function seq_disp()')
+        if len(self.sequence['keyframes']) == 0:
+            logging.info('No frames to display')
+            return
+        last_frame = max(self.sequence['keyframes'])
+        logging.debug('Last frame number %i:', last_frame)
+        for i in range(last_frame + 1):
+            print('Frame: %i / %i ' % (i, last_frame)),
+            print('- time:')
+        is_frame0(self.sequence)
+        
     def do_EOF(self, line):
         '''Override end of file'''
         logging.debug('Call function do_EOF()')
         logging.info('User has requested to stop the program')
-        stop = True
         print "Bye!"
         return True
 
