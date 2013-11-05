a75fa5960416bc890e556ae37ef06675e73d493a
Arn-O <rnd.bertrand@gmail.com>
2013-11-05 14:15:29 +0100

    add save/open

--- tools/sequence_editor.py
+++ tools/sequence_editor.py
@@ -10,6 +10,7 @@ Its purpose is to edit motion sequence (create/modify/save) and export
 a sequence script from call by the motor skills.
 '''
 
+import pickle
 import cmd
 import logging
 import argparse
@@ -17,6 +18,7 @@ import argparse
 # global constants
 DEF_FREQ = 60
 DEF_NB_JOINTS = 1
+DEF_FNAME = 'keyframes.txt'
 
 def get_parameters():
     ''' Manage the execution parameters'''        
@@ -35,17 +37,17 @@ def get_parameters():
         logging.basicConfig(level=logging.DEBUG)
     logging.info('Sequence editor started in maximum verbosity mode')
 
-def is_frame0(seq):
+def is_frame0(kf):
     '''Check if the frame 0 has been defined'''
     logging.debug('Call function is_frame0()')
-    if not 0 in seq['keyframes']:
+    if not 0 in kf['frames']:
         logging.warning('Frame0 not defined')
     else:
         logging.debug('Frame0 is defined')
 
 def parse_kframe_add(line):
     '''Parse the input for the add keyframe function'''
-    logging.debug('Call function parse_kframe_add()')
+    logging.debug('Call function parse_kframe_add()')   
     # TODO: add some checks here
     return int(line)
 
@@ -60,21 +62,24 @@ class SequenceEditor(cmd.Cmd):
         # initialize variables
         self.frequency = DEF_FREQ
         self.nb_joints = DEF_NB_JOINTS
-        self.sequence = {'frequency': self.frequency, 'keyframes': {}}
+        self.fname = DEF_FNAME
+        self.keyframes = {'frequency': self.frequency, 'frames': {}}
         print("'Crtl+D' or EOF to quit")
-        is_frame0(self.sequence)
+        is_frame0(self.keyframes)
         
-    def do_disp_param(self, line):
+    def do_param_disp(self, line):
         '''Display a list of the current parameters'''
-        logging.debug('Call function do_disp_param()')
+        logging.debug('Call function do_param_disp()')
         print('Frequency: %i' % self.frequency)
-        is_frame0(self.sequence)
+        print('Number of joint(s): %i' % self.nb_joints)
+        print('File name: %s' % self.fname)
+        is_frame0(self.keyframes)
 
     def do_kframe_disp(self, line):
         '''Insert a new keyframe'''
         logging.debug('Call function do_kframe_disp()')
-        print self.sequence
-        is_frame0(self.sequence)
+        print self.keyframes
+        is_frame0(self.keyframes)
         
     def do_kframe_add(self, line):
         '''Insert a new keyframe'''
@@ -87,21 +92,44 @@ class SequenceEditor(cmd.Cmd):
                     ((i + 1), self.nb_joints)
                 ))
             vals.append(val)
-        self.sequence['keyframes'][frame_nb] = vals
-        is_frame0(self.sequence)
+        self.keyframes['frames'][frame_nb] = vals
+        is_frame0(self.keyframes)
 
+    def do_kframe_save(self, line):
+        '''Save the current key frames'''
+        logging.debug('Call function do_kframe_save()')
+        f = open(self.fname, 'w')
+        pickle.dump(self.keyframes, f)
+        f.close()
+        is_frame0(self.keyframes)
+        
+    def do_kframe_open(self, line):
+        '''Open a key frames file'''
+        logging.debug('Call function do_kframe_open()')
+        f = open(self.fname, 'r')
+        self.keyframes = pickle.load(f) 
+        f.close()
+        logging.debug('Key frames loaded: %s', self.keyframes)
+        print('Run kframe_disp to check the content')
+        is_frame0(self.keyframes)
+                
     def do_seq_disp(self, line):
         '''Display the sequence of motion with interpolation'''
         logging.debug('Call function seq_disp()')
-        if len(self.sequence['keyframes']) == 0:
-            logging.info('No frames to display')
+        if len(self.keyframes['frames']) < 2:
+            logging.info('At least 2 keyframes required')
             return
-        last_frame = max(self.sequence['keyframes'])
+        last_frame = max(self.keyframes['frames'])
         logging.debug('Last frame number %i:', last_frame)
         for i in range(last_frame + 1):
             print('Frame: %i / %i ' % (i, last_frame)),
-            print('- time:')
-        is_frame0(self.sequence)
+            print('- time:'),
+            print('- type: '),
+            if i in keyframes['frames']:
+                print "K"
+            else:
+                print "I"
+        is_frame0(self.keyframes)
         
     def do_EOF(self, line):
         '''Override end of file'''
